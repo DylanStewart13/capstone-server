da2f921c6eb23062840c4eaeed6b6ad1020c3b89
dylanstewart13 <stewart.dylan.m@gmail.com>
2022-12-29 23:54:32 -0700

    added to methods

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const corsOptions = {
     origin: (origin, callback) => {
         callback(null, true);
     },
-    methods: ["GET,PUT,POST"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
     credentials: true
 };
