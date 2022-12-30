e4019640e08da4dc726f03764cef6ec591aff2ea
dylanstewart13 <stewart.dylan.m@gmail.com>
2022-12-29 22:20:26 -0700

    methods added options

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ let server;
 //CORS middleware
 var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     next();
 }
