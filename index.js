127b380f9e9745431edf58d2a031c0ebd6a5a741
dylanstewart13 <stewart.dylan.m@gmail.com>
2022-12-29 23:53:41 -0700

    removed methods

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const corsOptions = {
     origin: (origin, callback) => {
         callback(null, true);
     },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    methods: ["GET,PUT,POST"],
     allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
     credentials: true
 };
