10aa4f76270d765f1bc66012337165c779f2d022
dylanstewart13 <stewart.dylan.m@gmail.com>
2023-01-02 20:20:24 -0700

    updated cors once again

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.use('/api/quizzes/', quizRoutes);
 // };
 
 // app.options('*', cors(corsOptions));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors());
 
 
