52648467a0522c4991b1b8d813630b338d160391
dylanstewart13 <stewart.dylan.m@gmail.com>
2023-01-05 20:14:44 -0700

    cors

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 let server;
 
-// middlware configuration
-
-// CORS middleware
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://capstone--client.herokuapp.com');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Custom-Header, Accept, Authorization');
-    res.header('Access-Control-Expose-Headers', 'Cache-Control, Content-Language, Content-Length, Content-Type, Expires, Last-Modified, Pragma');
-    res.header('Access-Control-Max-Age: 600');
-    next();
-}
-app.use(cors());
-app.options('*', cors())
-app.use(allowCrossDomain);
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
 app.use(bodyParser.json({ limit: '20mb' }));
@@ -36,23 +21,15 @@ app.get("/*", (req, res) => { res.sendFile(path.join(__dirname, '/client/public/
 app.use('/api/users/', userRoutes);
 app.use('/api/quizzes/', quizRoutes);
 
-// const corsOptions = {
-//     origin: '*',
-//     callback(null, true);
-// },
-// methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-// allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-// credentials: true
-// };
+// CORS middleware
 
+const corsOptions = {
+    origin: 'https://capstone--client.herokuapp.com/*',
+    preflightContinue: false,
+}
 
-// const corsOptions = {
-//     origin: 'https://capstone--client.herokuapp.com/*',
-//     preflightContinue: false,
-// }
-
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.DB_URI, {
@@ -65,3 +42,26 @@ mongoose.connect(process.env.DB_URI, {
 server = app.listen(PORT, () => {
     console.log(`Node server running on port: ${PORT}`);
 });
+
+// other cors syntax already tried
+// const corsOptions = {
+//     origin: '*',
+//     callback(null, true);
+// },
+// methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+// allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+// credentials: true
+// };
+
+// var allowCrossDomain = function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'https://capstone--client.herokuapp.com');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Custom-Header, Accept, Authorization');
+//     res.header('Access-Control-Expose-Headers', 'Cache-Control, Content-Language, Content-Length, Content-Type, Expires, Last-Modified, Pragma');
+//     res.header('Access-Control-Max-Age: 600');
+//     next();
+// }
+// app.use(cors());
+// app.options('*', cors())
+// app.use(allowCrossDomain);
